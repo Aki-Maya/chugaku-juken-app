96de8e1e699226c3745d377f84fe6d3888b0c15f
Aki-Maya <mayamaakinori@gmail.com>
2025-06-23 21:12:27 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    GOOGLE_SHEETS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY,
+    GOOGLE_SHEETS_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEETS_ID,
+  },
 }
 
 module.exports = nextConfig
